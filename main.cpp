adbc13e751a1231892ebab2a70d3b2317d59b911
Christine Susic <csusic022@insite.4cd.edu>
2026-02-06 16:01:24 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+// COMSC-210 | Lab 3A | Christine Susic
+
+#include <iostream>
+using namespace std;
+
+struct Restaurant {
+    string name;
+    string address;
+    double ratingl
+    int hours;
+    int cost;
+};
+
+//prototypes
+void tempStruct();
+void outputStruct();
+
+int main() {
+    //calls functions
+    
+    return 0;
+}
+
+void tempStruct() {
+    //creates temporary struct
+    //receive user input to populate struct
+    //returns struct to main program
+}
+
+void outputStruct() {
+    //outputs struct data
+}
